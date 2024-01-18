48845635eb016c2919a33578c1c1680821ec96d9
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-01-18 10:05:51 -0300

    vite config ok

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `https://marianogarmendia.github.io/filter-products/`,
+  base: `https://marianogarmendia.github.io/filter-products`,
 });
 
 // export default {
