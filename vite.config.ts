49edc5941f172380c199eaf883738fd363ba5eb1
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-01-18 09:45:37 -0300

    vite config to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/marianogarmendia.github.io/`,
+});
+
+// export default {
+//   base: '/nombre-de-tu-repositorio/',
+//   // ... otras configuraciones
+// }
