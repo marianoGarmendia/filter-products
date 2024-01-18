32bcb44793fbf2927dcab2eb80c6917d9a137327
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-01-18 09:56:40 -0300

    config github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/marianogarmendia.github.io/`,
+  base: `https://marianogarmendia.github.io/filter-products/`,
 });
 
 // export default {
